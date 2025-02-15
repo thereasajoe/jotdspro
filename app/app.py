51714a35336bfee9cd98b385090bd3e441905a68
thereasajoe <23f3004024@ds.study.iitm.ac.in>
2025-02-15 19:54:15 +0530

    hi
    Signed-off-by: thereasajoe <23f3004024@ds.study.iitm.ac.in>

--- app/app.py
+++ app/app.py
@@ -19,8 +19,8 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import PlainTextResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from tasksA import *
-from tasksB import *
+from app.tasksA import *
+from app.tasksB import *
 import requests
 from dotenv import load_dotenv
 import os
